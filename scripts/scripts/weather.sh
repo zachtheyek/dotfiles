771d2c1e7f4e7c867959df41a1d3b016aa023bdb
zachtheyek <xiaoxiyek1.5@gmail.com>
2025-11-15 03:50:35 +0800

    Add logic to fetch reports at top of hour

--- scripts/scripts/weather.sh
+++ scripts/scripts/weather.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-while true; do 
+while true; do
+    echo "Hourly Weather Report: $(date)"
     curl wttr.in/Kuala-Lumpur
-    sleep 1800  # seconds
+
+    # Calculate seconds until the next hour
+    current_seconds=$(date +%s)
+    seconds_past_hour=$((current_seconds % 3600))
+    seconds_until_next_hour=$((3600 - seconds_past_hour))
+
+    sleep $seconds_until_next_hour
     clear
 done
