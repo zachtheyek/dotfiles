8342bb00368235fe2c317b49310fa65f3e2f0c3d
zachtheyek <xiaoxiyek1.5@gmail.com>
2025-11-21 11:30:18 +0800

    Remove asitop (too inefficient with resource management)

--- scripts/scripts/asitop_loop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while true; do
-    sudo asitop
-    echo "asitop exited, restarting in 5 seconds..."
-    echo "Press Ctrl+C to cancel restart."
-    sleep 5  # seconds
-    clear
-done
